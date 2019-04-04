02b648132494b4bd3e5f9b0ab58ca022779e78d1
fand <fand.gmork@gmail.com>
2019-04-05 02:01:54 +0900

    Remove unused tests

--- test/fixtures/hex-module.glsl
+++
@@ -1,5 +0,0 @@
-#pragma glslify: fake = require(glsl-fake-hex)
-
-void main() {
-  gl_FragColor = vec4(fake, 1.);
-}
--- test/fixtures/post-transform.glsl
+++
@@ -1,7 +0,0 @@
-#pragma glslify: fake = require(glsl-fake-post)
-
-// include 1
-
-void main() {
-  gl_FragColor = vec4(fake, 1.);
-}
